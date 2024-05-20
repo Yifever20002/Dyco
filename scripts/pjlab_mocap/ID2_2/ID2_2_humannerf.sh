265c475494d256c220be2722ebb403040e243ae0
某科学的苏打汽水 <75211407+Yifever20002@users.noreply.github.com>
2024-05-20 14:42:24 +0900

    Add files via upload

---
+++ scripts/pjlab_mocap/ID2_2/ID2_2_humannerf.sh
@@ -0,0 +1,13 @@
+# export CUDA_VISIBLE_DEVICES=0
+experiment=humannerf_baseline
+
+torchrun --nproc_per_node=4 --nnodes=1 --master_port 2950${CUDA_VISIBLE_DEVICES} train.py \
+    --cfg configs/human_nerf/ID2_2.yaml \
+    use_amp True \
+    frame_interval 3 \
+    train.lossweights.lpips 1.0 train.lossweights.mse 0.2 \
+    experiment ${experiment} \
+    non_rigid_motion_mlp.mlp_depth 6 \
+    canonical_mlp.mlp_depth 8 \
+    canonical_mlp.triplane False
+
